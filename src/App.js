1b9b9279299567444842a7fd75cf14bf6f69cab3
Belloshehu <belloshehu1@gmail.com>
2022-10-01 17:44:35 +0100

    Add Trusted by section
    resolve #3

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Header } from './components/Header';
 import { Hero } from './components/Hero';
+import { Trust } from './components/Trust';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
+      <Trust />
     </div>
   );
 }
